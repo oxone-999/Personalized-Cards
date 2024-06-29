e9b3f427c210fac6126888ff283753656d60ef1c
oxone-999 <anujverma11062002@gmail.com>
2024-06-30 01:58:12 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Appp from "./components/Card.jsx";
 
+
 function App() {
   return (
     <>
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,7 +1,10 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { Card, Image, Form, Button } from "react-bootstrap";
 import styled from "@emotion/styled";
 import "./cardbg.css";
+import Cropper from "./Cropper.tsx";
+import { canvasPreview } from "./canvasPreview";
+import { useDebounceEffect } from "./useDebounceEffect";
 
 const StyledCardImage = styled(Image)`
   object-fit: cover;
@@ -17,6 +20,12 @@ function Appp() {
   const [backgroundImage, setBackgroundImage] = useState("./images/def.jpg");
   const [paragraphText, setParagraphText] = useState("");
   const [fontSize, setFontSize] = useState(20);
+  const [completedCrop, setCompletedCrop] = useState()
+  const blobUrlRef = useRef("");
+  const imgRef = useRef(null)
+  const [scale, setScale] = useState(1);
+  const [rotate, setRotate] = useState(0)
+  const [aspect, setAspect] = useState(16 / 9)
 
   const handleImageUpload = (event) => {
     const imageFile = event.target.files[0];
@@ -41,15 +50,88 @@ function Appp() {
     setFontSize(newFontSize);
   };
 
+  async function onDownloadCropClick() {
+    const image = imgRef.current;
+    const previewCanvas = previewCanvasRef.current;
+    if (!image || !previewCanvas || !completedCrop) {
+      throw new Error("Crop canvas does not exist");
+    }
+
+    // This will size relative to the uploaded image
+    // size. If you want to size according to what they
+    // are looking at on screen, remove scaleX + scaleY
+    const scaleX = image.naturalWidth / image.width;
+    const scaleY = image.naturalHeight / image.height;
+
+    const offscreen = new OffscreenCanvas(
+      completedCrop.width * scaleX,
+      completedCrop.height * scaleY
+    );
+    const ctx = offscreen.getContext("2d");
+    if (!ctx) {
+      throw new Error("No 2d context");
+    }
+
+    ctx.drawImage(
+      previewCanvas,
+      0,
+      0,
+      previewCanvas.width,
+      previewCanvas.height,
+      0,
+      0,
+      offscreen.width,
+      offscreen.height
+    );
+    // You might want { type: "image/jpeg", quality: <0 to 1> } to
+    // reduce image size
+    const blob = await offscreen.convertToBlob({
+      type: "image/png",
+    });
+
+    if (blobUrlRef.current) {
+      URL.revokeObjectURL(blobUrlRef.current);
+    }
+    blobUrlRef.current = URL.createObjectURL(blob);
+
+    if (hiddenAnchorRef.current) {
+      hiddenAnchorRef.current.href = blobUrlRef.current;
+      hiddenAnchorRef.current.click();
+    }
+  }
+
+  useDebounceEffect(
+    async () => {
+      if (
+        completedCrop?.width &&
+        completedCrop?.height &&
+        imgRef.current &&
+        previewCanvasRef.current
+      ) {
+        // We use canvasPreview as it's much faster than imgPreview.
+        canvasPreview(
+          imgRef.current,
+          previewCanvasRef.current,
+          completedCrop,
+          scale,
+          rotate
+        );
+      }
+    },
+    100,
+    [completedCrop, scale, rotate]
+  );
+
   return (
     <div className="card">
       <div className="card-body">
         <div className="form-group mb-3">
-          <label for="formFile">Upload Image</label>
-          <input type="file" id="formFile" onChange={handleImageUpload} />
+          {/* <label for="formFile">Upload Image</label>
+          <input type="file" id="formFile" onChange={handleImageUpload} /> */}
+          <Cropper />
         </div>
         <div className="form-group">
-          <label for="formText">Paragraph</label>
+          <label>Paragraph</label>
           <textarea
             id="formText"
             rows={3}
@@ -74,7 +156,41 @@ function Appp() {
         <div className="cardbg">
           <img src="./images/Card BG.png" />
           <div className="image">
-            <img src={backgroundImage} alt="Card background" />
+            {!!completedCrop && (
+              <>
+                <div>
+                  <canvas
+                    ref={previewCanvasRef}
+                    style={{
+                      border: "1px solid black",
+                      objectFit: "contain",
+                      width: completedCrop.width,
+                      height: completedCrop.height,
+                    }}
+                  />
+                </div>
+                <div>
+                  <button onClick={onDownloadCropClick}>Download Crop</button>
+                  <div style={{ fontSize: 12, color: "#666" }}>
+                    If you get a security error when downloading try opening the
+                    Preview in a new tab (icon near top right).
+                  </div>
+                  <a
+                    href="#hidden"
+                    ref={hiddenAnchorRef}
+                    download
+                    style={{
+                      position: "absolute",
+                      top: "-200vh",
+                      visibility: "hidden",
+                    }}
+                  >
+                    Hidden download
+                  </a>
+                </div>
+              </>
+            )}
+            {/* <img src={backgroundImage} alt="Card background" /> */}
           </div>
           <div className="para">
             <p style={{ fontSize: `${fontSize}px` }}>{paragraphText}</p>

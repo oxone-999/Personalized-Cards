676749a91bd57758db7c6d67387fee24d68b914c
oxone-999 <anujverma11062002@gmail.com>
2024-07-03 00:51:25 +0530

    add

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -272,8 +272,8 @@ function Appp() {
                   style={{
                     border: "1px solid black",
                     objectFit: "contain",
-                    width: completedCrop.width,
-                    height: completedCrop.height,
+                    width: completedCrop.width*1.5,
+                    height: completedCrop.height*1.5,
                   }}
                 />
               </div>
